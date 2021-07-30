68f0495bb3f27a1c362b55a6c023926a8de78076
ThelyGit <t_eliseu@yahoo.com>
2021-07-30 10:52:08 +0300

    Single line commment added

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -1,7 +1,7 @@
 package ro.java.ctrln;
 
 public class Main {
-
+    // aceasta este metoda Main care porneste o aplicatie Java
     public static void main(String[] args) {
 
         System.out.println("Hai ca merge pana la urma!!");
